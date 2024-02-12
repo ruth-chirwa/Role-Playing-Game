ce163f5c970e3f5808d6dd9162cb8fc8c93c006d
ruth-chirwa <chirwaruth647@gmail.com>
2024-02-12 15:28:45 -0500

    added a keyword, an array, and variables

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+console.log("My first javascript line of code!");
+console.log("Isn't this cool!!");
+let xp = 0;
+console.log(xp);
+let health = 100;
+let gold = 50;
+/*When a variable name has multiple words, the convention in JavaScript is to use what's called camelCase. 
+The first word is lowercase, and the first letter of every following word is uppercase.*/
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory;
+
+/*The variables you have assigned have all had values that are numbers.
+ JavaScript has multiple different data types. 
+ The next one you will use is the string. 
+ Strings are used to store things like words or text.
+ Strings are surrounded with double quotes, single quotes, or backticks. */
+ let name = "Ruth";
+console.log(name);
+//You will need to use a data type that can do this. 
+//An array can be used to hold multiple values.
+let numbers = ["one", "two", "three"];
+console.log(numbers);
