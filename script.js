0aaf1c3b73e17ca2c25b131cd1329b9d88b4f624
ruth-chirwa <chirwaruth647@gmail.com>
2024-02-21 07:34:45 -0500

    added another object

--- script.js
+++ script.js
@@ -109,8 +109,22 @@ function goStore() {
   const locations = [
     {
       name: "town square",
-      "button text": ["Go to store", "Go to cave", "Fight dragon"],
-      "button functions": [goStore, goCave, fightDragon],
-      "text": "You are in the town square. You see a sign that says \"Store\"."
+      "button text": ["Go to store", 
+      "Go to cave", 
+      "Fight dragon"],
+      "button functions": [goStore, 
+      goCave, 
+      fightDragon],
+      text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {  
+      name: "store",
+       "button text": ["Buy 10 health (10 gold)", 
+       "Buy weapon (30 gold)", 
+       "Go to town square"],
+      "button functions": [buyHealth, 
+      buyWeapon, 
+      goTown],
+      text: "You enter the store."
     }
   ];
