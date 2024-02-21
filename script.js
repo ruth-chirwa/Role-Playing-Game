6e0dd8563c55a46660be0274c364130e3925e3af
ruth-chirwa <chirwaruth647@gmail.com>
2024-02-21 07:27:22 -0500

    added key:value pairs with arrays

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ function goStore() {
     {
       name: "town square",
       "button text": ["Go to store", "Go to cave", "Fight dragon"],
-      "button functions": [goStore, goCave, fightDragon]
+      "button functions": [goStore, goCave, fightDragon],
+      "text": "You are in the town square. You see a sign that says \"Store\"."
     }
   ];
